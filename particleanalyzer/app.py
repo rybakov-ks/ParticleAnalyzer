35c79a4dbcb9f294fc87e1517a2b6cab3e98a7e9
rybakov-ks <57319102+rybakov-ks@users.noreply.github.com>
2025-06-13 21:20:34 +0400

    Update

--- particleanalyzer/app.py
+++ particleanalyzer/app.py
@@ -1,9 +1,7 @@
 ﻿# -*- coding: utf-8 -*-
-import os
 from particleanalyzer.core.ui import create_interface
 from particleanalyzer.core.languages import i18n
 
-example_path = os.path.join(os.path.dirname(__file__), "assets", "example")
 
 def main():
     demo = create_interface()
@@ -15,6 +13,5 @@ def main():
         server_port=8000,
         pwa=True,
         favicon_path="particleanalyzer/assets/icon/search-solid.png",
-        i18n=i18n,
-        allowed_paths=[example_path]
+        i18n=i18n
     )
