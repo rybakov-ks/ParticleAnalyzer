68a6c16ca749119bd0bcec85f9b70b515f27954e
rybakov-ks <57319102+rybakov-ks@users.noreply.github.com>
2025-06-13 11:53:11 +0400

    Update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename):
 
 setup(
     name="ParticleAnalyzer",
-    version="0.1.11", 
+    version="0.1.12", 
     packages=find_packages(exclude=['tests*']),
     package_data={
         'particleanalyzer': [
