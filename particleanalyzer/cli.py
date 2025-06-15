54eb45bffb25b7acc2a598f00955e15e5858f146
rybakov-ks <57319102+rybakov-ks@users.noreply.github.com>
2025-06-15 15:06:55 +0400

    Translation dictionary updated and added the ability to set a port

--- particleanalyzer/cli.py
+++ particleanalyzer/cli.py
@@ -6,12 +6,17 @@ def main():
     parser = argparse.ArgumentParser(description='Particle Analyzer')
     subparsers = parser.add_subparsers(dest='command', required=True)
     
-    # Команда run
     run_parser = subparsers.add_parser('run', help='Run the application')
+    run_parser.add_argument(
+        '--port',
+        type=int,
+        default=8000,
+        help='Port to run the application on (default: 8000)'
+    )
     run_parser.set_defaults(func=run_app)
     
     args = parser.parse_args()
-    args.func()
+    args.func(port=args.port)
 
 if __name__ == "__main__":
     main()
